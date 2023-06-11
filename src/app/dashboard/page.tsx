98606afd8d93597359b9b14e8da7122702e476a4
Bruno-mateus <bruno_205@hotmail.com>
2023-06-11 18:19:21 -0300

    new app route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl">Home</h1>
+      <h1 className="text-4xl">Dashboard</h1>
       <Link
-        href={"/dashboard"}
         className="text-white font-bold hover:bg-green-500 rounded-lg p-4 bg-green-600"
+        href={"/"}
       >
-        Dashboard
+        Home
       </Link>
     </main>
   );
